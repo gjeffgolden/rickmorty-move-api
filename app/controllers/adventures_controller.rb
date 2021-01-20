b8095dc2dcf5ddd571231f5c61b22e695bf246cc
bwesterg <benton.westergaard@gmail.com>
2021-01-20 15:55:42 -0700

    adventure method built

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,22 @@
 class AdventuresController < ApplicationController
+
+    def index
+        @adventures = Adventure.all 
+        render json: @adventures, include: [:character_id, :location_id]
+    end
+
+    def show
+        @adventure = Adventure.find( params[:id])
+        render json: @adventure, include: [:character_id, :location_id]
+    end
+
+
+    def Create 
+        @adventure = Adventure.create(
+            character_id: params[:character_id],
+            location_id: params[:location_id]
+        )
+        render json: @adventure
+    end
+
 end
